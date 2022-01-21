0eeacfbfb762fccea4c9d0d056bbe2ca960cb35d
hemangchaudhry <hemangchaudhry25@gmail.com>
2022-01-21 18:19:52 +0530

    birthday reminder

---
+++ birthday reminder/src/data.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 1,
+    name: "Bertie Yates",
+    age: 29,
+    image:
+      "https://i0.wp.com/static1.e621.net/data/sample/95/d5/95d583b99d46118b0cc0fab108f46b10.jpg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 32,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCNHHRpD6tExQLobjm7zs8sBoyi0Xio9Ht8g&usqp=CAU",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image: "https://links24.me/images/extra_thumb/79/718.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+]
