8a7046ac06a2b23a94b4ffb35df0aeec9ccc6762
hemangchaudhry <hemangchaudhry25@gmail.com>
2022-01-21 23:04:46 +0530

    review and accordian

---
+++ accordian/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react"
+import questions from "./data"
+// import data from "./data"
+import SingleQuestion from "./Question"
+function App() {
+  // const [question, setQuestion] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>question and answers</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            )
+          })}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App
